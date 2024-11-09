9c2774f2857de041f3aff0b38bbc1f3110af0c18
Amanullah202 <malikamanullah202@gmail.com>
2024-11-09 15:04:46 +0500

    fhfhf

--- server.js
+++ server.js
@@ -255,9 +255,9 @@ io.on("connection", (socket) => {
   });
 });
 io.engine.on("connection_error", (err) => {
-  console.log(err.req); // the request object
+  // console.log(err.req); // the request object
   // console.log(err.code); // the error code, for example 1
-  // console.log(err.message); // the error message, for example "Session ID unknown"
+  console.log(err.message); // the error message, for example "Session ID unknown"
 });
 // Start the server
 const PORT = process.env.PORT || 4040;
